9d660e8f7eec589d209031c6a37ec733627ff716
宋青松 <suvlliansong@163.com>
2017-01-30 20:08:18 +0800

    fn对象部分:shit:

--- Szepto.js
+++ Szepto.js
@@ -5,8 +5,24 @@ var sZepto = (function() {
 		class2type = {},
 		toString = class2type.toString,
 		zepto = {},
+		tempParent = document.createElement('div'),
 		isArray = Array.isArray || 
 			function(object) { return object instanceof Array };
+
+	zepto.matches = function(element, selector) {
+		if (!selector || !element || element.nodeType !== 1) { return false; }
+		var matchesSelector = element.matches || element.webkitMatchesSelector ||
+								element.mozMatchesSelector || element.oMatchesSelector ||
+								element.matchesSelector;
+		if (matchesSelector) { return matchesSelector.call(element, selector); }
+		var match, parent = element.parentNode, temp = !parent;
+		if (temp) {
+			(parent = tempParent).appendChild(element);
+			match = ~zepto.qsa(parent, selector).indexOf(element)
+			temp && tempParent.removeChild(element);
+			return match;			
+		}
+	}
 
 	function type(obj) {
 		return obj == null ? String(obj) : 
@@ -22,8 +38,23 @@ var sZepto = (function() {
 		return isObject(obj) && !isWindow(obj) && Object.getPrototypeOf(obj) == Object.prototype;
 	}
 
+	// 类数组类型
+	function likeArray(obj) {
+		var length = !!obj && 'length' in obj && obj.length,
+			type = $.type(obj);
+
+		return 'function' != type && !isWindow(obj) &&(
+			'array' == type || length === 0 || 
+			(typeof length == 'number' && length > 0 && (length - 1) in obj)	
+		);
+ 	}
+
 	// 返回数组中不为空的项
-	function compact(array) { return filter.call(array, function(item) { return item != null;})}
+	function compact(array) { return filter.call(array, function(item) { return item != null; }); }
+	function flatten(array) { return array.length > 0 ? $.fn.concat.apply([], array) : array; }
+
+	// 返回给定值和键值的值
+	uniq = function(array) { return filter.call(array, function(item, index) { return array.indexOf(item) == index; }); }
 
 	function Z(dom,selector) {
 		var i = 0,len = dom ? dom.length : 0;
@@ -45,7 +76,7 @@ var sZepto = (function() {
 	zepto.init = function(selector, context) {
 		var dom;
 		// 没有给出selector，返回一个空的Zepto collection
-		if(!selector) return zepto.Z();
+		if(!selector) { return zepto.Z(); }
 		// selector是一个字符串
 		else if (typeof selector == 'string') {
 			selector = selector.trim();
@@ -101,6 +132,17 @@ var sZepto = (function() {
 		}
 	}
 
+	$.extend = function(target) {
+		var deep, args = slice.call(arguments, 1);
+		if (typeof target == 'boolean') {
+			deep = target;
+			target = args.shift();
+		}
+		args.forEach(function(arg) { extend(target, arg, depp); });
+		return target;
+	}
+
+
 	zepto.qsa = function(element, selector) {
 		var found,
 			maybeID = selector[0] == "#",
@@ -116,5 +158,162 @@ var sZepto = (function() {
 				element.getElementByTagName(selector) :
 				element.querySelectorAll(selector)
 			);
+	}
+
+	$.contains = document.docmentElement.contains ? 
+		function(parent, node) {
+			return parent !== node && parent.contains(node);
+		} :
+		function(parent, node) {
+			while (node && (node = node.parentNode)){
+				if (node === parent) { return true; }
+			}
+			return false;
+		}
+
+	// 将函数指针赋值给“$”，使其称为“$”的属性
+	$.type = type;
+	$.isFunction = isFunction;
+	$.isWindow = isWindow;
+	$.isArray = isArray;
+	$.isPlainObject = isPlainObject;
+
+	$.noop = function() {}
+
+	$.each = function(elements, callback) {
+		var i, key;
+		if (likeArray(elements)) {
+			for (i = 0; i < elements.length; i++) {
+				if (callback.call(elements[i], i, elements[i]) === false) { return elements; }
+			}
+		} else {
+			for (key in elements) {
+				if (callback.call(elements[key], key, elements[key]) === false) { return elements; }
+			}
+		}
+		return elements;
+	}
+
+	$.each("Boolean Number String Function Array Date RegExp Object Error".split(" "), function(i, name) {
+		class2type[ "[object " + name + "]"] = name.toLowerCase();
+	})
+
+	$.isEmptyObject = function(obj) {
+		var name;
+		for (name in obj) { return false; }
+		return true;
+	}
+
+	$.isNumberic = function(val) {
+		var num = Number(val), type = typeof val;
+		return val != null && type != 'boolean' && 
+			(type != 'string' || val.length) && 
+			!isNaN(num) && isFinite(num) || false;
+	}
+
+	$.inArray = function(elem, array, i) {
+		return emptyArray.indexOf.call(array, elem, i);
+	}
+
+	$.trim = function(str) {
+		return str == null ? "" : String.prototype.trim.call(str);
+	}
+
+	$.map = function(elements, callback) {
+		var value, values = [], i, key;
+		if (likeArray(elements)) {
+			for (i = 0; i < elements.length; i++) {
+				value = callback(elements[i], i);
+				if (value != null) { values.push(value); }
+			}
+		}
+		else {
+			for (key in elements) {
+				value = callback(elements[key], key);
+				if (value != null) { values.push(value); }
+			}
+		}
+		return flatten(values);
+	}
+
+	$.fn = {
+		constructor: zepto.Z,
+		length: 0,
+
+		// 函数指针
+		forEach: emptyArray.forEach,
+		reduce: emptyArray.reduce,
+		push: emptyArray.push,
+		sort: emptyArray.sort,
+		splice: emptyArray.splice,
+		indexOf: emptyArray.indexOf,
+
+		concat: function() {
+			var i, value, args =[];
+			for (i = 0; i < arguments.length; i++) {
+				value = arguments[i];
+				args[i] = zepto.isZ(value) ? value.toArray() : value;
+			}
+			return concat.apply(zepto.isZ(this) ? this.toArray() : this, args);
+		},
+
+		get: function(index) {
+			return index === undefined ? slice.call(this) : this[index >= 0 ? index : index + this.length];
+		},
+
+		add: function(selector, context) { return $(uniq(this.concat($(selector, context)))); },
+
+		slice: function() { return $(slice.apply(this, arguments)); },
+
+		toArray: function(){ return this.get();	},
+
+		size: function() { return this.length; },
+
+		map: function(fn) { return $($.map(this, function(el, i){ return fn.call(el, i, el) })); },
+
+		is: function(selector) { return this.length > 0 && zepto.matches(this[0], selector); },
+
+		eq: function(index) { return index === -1 ? this.slice(index) : this.slice(index, + index + 1) },
+
+		first: function() {
+			var el = this[0];
+			return el && !isObject(el) ? el : $(el);
+		},
+
+		last: function() {
+			var el = this[this.length - 1];
+			return el && !isObject(el) ? el : $(el);
+		}
+
+		remove: function() {
+			return this.each(function() {
+				if(this.parentNode != null) { 
+					this.parentNode.removeChild(this);
+				}
+			})
+		},
+
+		each: function(callback) {
+			emptyArray.every.call(this, function(el, index) {
+				return callback.call(el, index,el) !== false;
+			});
+			return this;
+		}
+
+		find: function(selector) {
+			var result, $this = this;
+			if (!selector) { result = $(); }
+			else if (typeof selector == 'object') {
+				result = $(selector).filter(function(){
+					var node = this;
+					return emptyArray.some.call($this, function(parent){
+						return $.contains(parent, node);
+					})
+				})
+			}
+			else if (this.length == 1) { result = $(zepto.qsa(this[0], selector)); }
+			else { result = this.map(function() { retu zepto.qsa(this, selector); }); }
+			return result;
+		},
 	}
 })
